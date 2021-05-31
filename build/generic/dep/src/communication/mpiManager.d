6873af42ac79db7eda40fe43080f06825213e6ad
Ramiro Freile <freiler@Administrators-MacBook-Pro.local>
2021-05-31 16:33:42 -0500

    first commit

---
+++ build/generic/dep/src/communication/mpiManager.d
@@ -0,0 +1,1 @@
+build/generic/obj/src/communication/mpiManager.o: src/communication/mpiManager.cpp
---
+++ build/generic/dep/src/communication/ompManager.d
@@ -0,0 +1,1 @@
+build/generic/obj/src/communication/ompManager.o: src/communication/ompManager.cpp
